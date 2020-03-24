976f0e6c9bf4c885bedbbbf85568585103835cbf
Johan Sjöblom <johan.sjoblom@squeed.com>
2020-03-24 14:44:56 +0100

    More and better tests

--- arkivering-end-to-end-tests/src/test/kotlin/no/nav/soknad/arkivering/arkiveringendtoendtests/ArkiveringEndToEndTestsApplicationTests.kt
+++ arkivering-end-to-end-tests/src/test/kotlin/no/nav/soknad/arkivering/arkiveringendtoendtests/ArkiveringEndToEndTestsApplicationTests.kt
@@ -37,6 +37,7 @@ class ArkiveringEndToEndTestsApplicationTests {
 	@BeforeEach
 	fun setup() {
 		checkThatDependenciesAreUp()
+		resetJoark()
 	}
 
 	private fun checkThatDependenciesAreUp() {
@@ -59,20 +60,131 @@ class ArkiveringEndToEndTestsApplicationTests {
 		sendDataToMottaker(dto)
 
 		verifyDataInJoark(dto)
+		verifyNumberOfCallsToJoark(1)
 	}
 
+	@Test
+	fun `Happy case - several files in file storage`() {
+		val uuid0 = UUID.randomUUID().toString()
+		val uuid1 = UUID.randomUUID().toString()
+		val dto = SoknadInnsendtDto(UUID.randomUUID().toString(), false, "personId", "tema", LocalDateTime.now(),
+			listOf(
+				InnsendtDokumentDto("NAV 10-07.17", true, "Søknad om refusjon av reiseutgifter - bil",
+					listOf(InnsendtVariantDto(uuid0, null, "filnavn", "1024", "variantformat", "PDFA"))),
+
+				InnsendtDokumentDto("NAV 10-07.17", false, "Søknad om refusjon av reiseutgifter - bil",
+					listOf(InnsendtVariantDto(uuid1, null, "filnavn", "1024", "variantformat", "PDFA")))
+				))
+
+		sendFilesToFileStorage(uuid0)
+		sendFilesToFileStorage(uuid1)
+		sendDataToMottaker(dto)
+
+		verifyDataInJoark(dto)
+		verifyNumberOfCallsToJoark(1)
+	}
+
+	@Test
+	fun `No files in file storage - Nothing is sent to Joark`() {
+		val dto = createDto(UUID.randomUUID().toString())
+
+		sendDataToMottaker(dto)
+
+		verifyDataNotInJoark(dto)
+		verifyNumberOfCallsToJoark(0)
+	}
+
+	@Test
+	fun `Several Hovedskjemas - Nothing is sent to Joark`() {
+		val uuid0 = UUID.randomUUID().toString()
+		val uuid1 = UUID.randomUUID().toString()
+		val dto = SoknadInnsendtDto(UUID.randomUUID().toString(), false, "personId", "tema", LocalDateTime.now(),
+			listOf(
+				InnsendtDokumentDto("NAV 10-07.17", true, "Søknad om refusjon av reiseutgifter - bil",
+					listOf(InnsendtVariantDto(uuid0, null, "filnavn", "1024", "variantformat", "PDFA"))),
+
+				InnsendtDokumentDto("NAV 10-07.17", true, "Søknad om refusjon av reiseutgifter - bil",
+					listOf(InnsendtVariantDto(uuid1, null, "filnavn", "1024", "variantformat", "PDFA")))
+			))
+
+		sendFilesToFileStorage(uuid0)
+		sendFilesToFileStorage(uuid1)
+		sendDataToMottaker(dto)
+
+		verifyDataNotInJoark(dto)
+		verifyNumberOfCallsToJoark(0)
+	}
+
+	@Test
+	fun `Joark responds 404 on first two attempts - Works on third attempt`() {
+		val uuid = UUID.randomUUID().toString()
+		val dto = createDto(uuid)
+
+		sendFilesToFileStorage(uuid)
+		mockJoarkRespondsWithCodeForXAttempts(404, 2)
+		sendDataToMottaker(dto)
+
+		verifyDataInJoark(dto)
+		verifyNumberOfCallsToJoark(3)
+	}
+
+	@Test
+	fun `Joark responds 500 on first attempt - Works on second attempt`() {
+		val uuid = UUID.randomUUID().toString()
+		val dto = createDto(uuid)
+
+		sendFilesToFileStorage(uuid)
+		mockJoarkRespondsWithCodeForXAttempts(500, 1)
+		sendDataToMottaker(dto)
+
+		verifyDataInJoark(dto)
+		verifyNumberOfCallsToJoark(2)
+	}
+
+	@Test
+	fun `Joark responds 200 but has wrong response body - What happens?`() { // TODO
+
+	}
+
+	private fun resetJoark() {
+		val url = "http://localhost:${dependencies["joark-mock"]}/joark/mock/response-behaviour/reset"
+		restTemplate.put(url, null)
+	}
+
+	private fun mockJoarkRespondsWithCodeForXAttempts(status: Int, forAttempts: Int) {
+		val url = "http://localhost:${dependencies["joark-mock"]}/joark/mock/response-behaviour/$status/$forAttempts"
+		restTemplate.put(url, null)
+	}
+
+	private fun verifyDataNotInJoark(dto: SoknadInnsendtDto) {
+		val key = dto.innsendingsId
+		val url = "http://localhost:${dependencies["joark-mock"]}/joark/lookup/$key"
+
+		val responseEntity: ResponseEntity<List<LinkedHashMap<String, String>>> = pollJoarkUntilTimeout(url)
+
+		if (responseEntity.hasBody())
+			fail("Expected Joark to not have any results for $key")
+	}
+
+	private fun verifyNumberOfCallsToJoark(expectedNumberOfCalls: Int) {
+		val url = "http://localhost:${dependencies["joark-mock"]}/joark/mock/response-behaviour/numberOfCalls"
+		val responseEntity = restTemplate.getForEntity(url, Int::class.java)
+		assertEquals(expectedNumberOfCalls, responseEntity.body)
+	}
 
 	private fun verifyDataInJoark(dto: SoknadInnsendtDto) {
 		val key = dto.innsendingsId
 		val url = "http://localhost:${dependencies["joark-mock"]}/joark/lookup/$key"
 
-		val responseEntity: ResponseEntity<List<LinkedHashMap<String, String>>> = pollJoarkUntilResponds(url)
+		val responseEntity: ResponseEntity<List<LinkedHashMap<String, String>>> = pollJoarkUntilTimeout(url)
 
+		if (!responseEntity.hasBody())
+			fail("Failed to get response from Joark")
 		assertEquals(dto.tema, responseEntity.body?.get(0)!!["message"])
 		assertEquals(dto.innsendingsId, responseEntity.body?.get(0)!!["name"])
 	}
 
-	private fun <T> pollJoarkUntilResponds(url: String): ResponseEntity<List<T>> {
+	private fun <T> pollJoarkUntilTimeout(url: String): ResponseEntity<List<T>> {
 
 		val respType = object : ParameterizedTypeReference<List<T>>() {}
 
@@ -87,7 +199,7 @@ class ArkiveringEndToEndTestsApplicationTests {
 			}
 			TimeUnit.MILLISECONDS.sleep(50)
 		}
-		fail("Failed to get response from Joark")
+		return ResponseEntity.of(Optional.empty())
 	}
 
 	private fun sendFilesToFileStorage(uuid: String) {
@@ -109,7 +221,7 @@ class ArkiveringEndToEndTestsApplicationTests {
 		restTemplate.postForObject(url, request, String::class.java)
 	}
 
-	private fun createDto(uuid: String) = SoknadInnsendtDto("innsendingId", false, "personId", "tema", LocalDateTime.now(),
+	private fun createDto(uuid: String) = SoknadInnsendtDto(UUID.randomUUID().toString(), false, "personId", "tema", LocalDateTime.now(),
 		listOf(InnsendtDokumentDto("NAV 10-07.17", true, "Søknad om refusjon av reiseutgifter - bil",
 			listOf(InnsendtVariantDto(uuid, null, "filnavn", "1024", "variantformat", "PDFA")))))
 }
