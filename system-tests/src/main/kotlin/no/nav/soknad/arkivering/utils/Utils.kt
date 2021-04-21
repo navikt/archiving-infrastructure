dd81e2f4695b06e987841cfb7abe34614666478a
Johan Sjöblom <johan.sjoblom@squeed.com>
2021-04-21 10:53:39 +0200

    Sending more real data

--- system-tests/src/main/kotlin/no/nav/soknad/arkivering/utils/Utils.kt
+++ system-tests/src/main/kotlin/no/nav/soknad/arkivering/utils/Utils.kt
@@ -25,12 +25,11 @@ fun loopAndVerify(expectedCount: Int, getCount: () -> Int,
 }
 
 
-fun createDto(fileId: String, innsendingsId: String = UUID.randomUUID().toString()) =
-	SoknadInnsendtDto(innsendingsId, false, "personId", "tema", LocalDateTime.now(),
-		listOf(InnsendtDokumentDto("NAV 10-07.17", true, "Søknad om refusjon av reiseutgifter - bil",
-			listOf(InnsendtVariantDto(fileId, null, "filnavn", "1024", "variantformat", "PDFA")))))
+private const val fnr = "10108000398" // Not a real fnr
 
-fun createDto(fileIds: List<String>) =
-	SoknadInnsendtDto(UUID.randomUUID().toString(), false, "personId", "tema", LocalDateTime.now(),
+fun createDto(fileId: String, innsendingsId: String = UUID.randomUUID().toString()) = createDto(listOf(fileId), innsendingsId)
+
+fun createDto(fileIds: List<String>, innsendingsId: String = UUID.randomUUID().toString()) =
+	SoknadInnsendtDto(innsendingsId, false, fnr, "BIL", LocalDateTime.now(),
 		listOf(InnsendtDokumentDto("NAV 10-07.17", true, "Søknad om refusjon av reiseutgifter - bil",
-			fileIds.map { InnsendtVariantDto(it, null, "filnavn", "1024", "variantformat", "PDFA") })))
+			fileIds.map { InnsendtVariantDto(it, null, "filnavn", "1024", "ARKIV", "PDFA") })))
