940fbd9e347db35c6f288b472204ceb4a69258d6
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-08-10 15:42:43 +0200

    Adding loadtest

--- system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/LoadTests.kt
+++ system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/LoadTests.kt
@@ -32,6 +32,16 @@ class LoadTests(config: Config, kafkaConfig: KafkaConfig) {
 
 
 	@Suppress("FunctionName")
+	fun `5000 simultaneous entities, 1 times 1 MB each`() {
+		val testName = Thread.currentThread().stackTrace[1].methodName
+		val numberOfEntities = 5000
+		val numberOfFilesPerEntity = 1
+		val file = fileOfSize1mb
+
+		performTest(testName, numberOfEntities, numberOfFilesPerEntity, file)
+	}
+
+	@Suppress("FunctionName")
 	fun `100 simultaneous entities, 2 times 2 MB each`() {
 		val testName = Thread.currentThread().stackTrace[1].methodName
 		val numberOfEntities = 100
@@ -54,7 +64,7 @@ class LoadTests(config: Config, kafkaConfig: KafkaConfig) {
 	@Suppress("FunctionName")
 	fun `10 000 simultaneous entities, 1 times 1 byte each`() {
 		val testName = Thread.currentThread().stackTrace[1].methodName
-		val numberOfEntities = 5000
+		val numberOfEntities = 10_000
 		val numberOfFilesPerEntity = 1
 		val file = fileOfSize1byte
 
