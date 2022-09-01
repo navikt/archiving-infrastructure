9e79ba4a4e09ab2d7d4900348689916909a61dac
Johan Sjöblom <johan.sjoblom@squeed.com>
2022-09-01 16:45:59 +0200

    Cleaning up OAuth2 code

--- system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/tokensupport/DefaultOAuth2HttpClient.kt
+++ system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/tokensupport/DefaultOAuth2HttpClient.kt
@@ -1,41 +1,29 @@
 package no.nav.soknad.arkivering.tokensupport
 
-
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import no.nav.security.token.support.client.core.OAuth2ClientException
 import no.nav.security.token.support.client.core.http.OAuth2HttpClient
 import no.nav.security.token.support.client.core.http.OAuth2HttpRequest
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
-import okhttp3.*
-import org.slf4j.LoggerFactory
-import java.net.URL
-
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class DefaultOAuth2HttpClient(private val client: OkHttpClient) : OAuth2HttpClient {
-
-	private val logger = LoggerFactory.getLogger(javaClass)
 
 	override fun post(oAuth2HttpRequest: OAuth2HttpRequest): OAuth2AccessTokenResponse {
 
 		val bodyBuilder = FormBody.Builder()
+		oAuth2HttpRequest.formParameters.forEach { (k, v) -> bodyBuilder.add(k, v) }
 
-		oAuth2HttpRequest.formParameters.forEach{ (k, v) -> bodyBuilder.add(k,v)}
+		val requestBuilder = Request.Builder().url(oAuth2HttpRequest.tokenEndpointUrl.toURL())
+		oAuth2HttpRequest.oAuth2HttpHeaders.headers()
+			.forEach { (k, v) -> requestBuilder.header(k, v.joinToString(separator = ",")) }
 
-		val requestBuilder = Request.Builder().url( oAuth2HttpRequest.tokenEndpointUrl.toURL())
-
-		oAuth2HttpRequest.oAuth2HttpHeaders.headers().forEach{ (k,v) -> requestBuilder.header(k, v.joinToString(separator = ","))}
-
-    val request = requestBuilder.post(bodyBuilder.build()).build()
-
+		val request = requestBuilder.post(bodyBuilder.build()).build()
 		val response = client.newCall(request).execute()
-		val mapper = ObjectMapper()
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false)
-		logger.info("Response body from Oauth2 exchange is " + response.body)
-		return mapper.readValue(response.body?.bytes(),OAuth2AccessTokenResponse::class.java)
 
+		val mapper = ObjectMapper().also { it.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) }
+		return mapper.readValue(response.body?.bytes(), OAuth2AccessTokenResponse::class.java)
 	}
-
-
-
 }
---
+++ system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/tokensupport/OkHttpAuthorizationClient.kt
@@ -0,0 +1,43 @@
+package no.nav.soknad.arkivering.tokensupport
+
+import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod
+import no.nav.security.token.support.client.core.ClientAuthenticationProperties
+import no.nav.security.token.support.client.core.ClientProperties
+import no.nav.security.token.support.client.core.OAuth2GrantType
+import no.nav.security.token.support.client.core.oauth2.ClientCredentialsTokenClient
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
+import no.nav.soknad.arkivering.OAuth2Config
+import okhttp3.OkHttpClient
+import java.net.URI
+
+fun createOkHttpAuthorizationClient(scopesProvider: (OAuth2Config) -> List<String>): OkHttpClient {
+	val oauth2Conf = OAuth2Config()
+	val oauth2AccessTokenService = OAuth2AccessTokenService(
+		null,
+		null,
+		ClientCredentialsTokenClient(DefaultOAuth2HttpClient(OkHttpClient())),
+		null
+	)
+	val clientProperties = ClientProperties(
+		URI.create(oauth2Conf.tokenEndpointUrl),
+		null, OAuth2GrantType(oauth2Conf.grantType),
+		scopesProvider.invoke(oauth2Conf),
+		ClientAuthenticationProperties(
+			oauth2Conf.clientId,
+			ClientAuthenticationMethod(oauth2Conf.clientAuthMethod),
+			oauth2Conf.clientSecret,
+			null
+		), null, null
+	)
+	val tokenService = TokenService(clientProperties, oauth2AccessTokenService)
+
+	val okHttpClientTokenService = OkHttpClient().newBuilder().addInterceptor {
+		val token = tokenService.getToken()
+		val bearerRequest = it.request().newBuilder().headers(it.request().headers)
+			.header("Authorization", "Bearer $token").build()
+
+		it.proceed(bearerRequest)
+	}.build()
+
+	return okHttpClientTokenService
+}
