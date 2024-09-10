797fc58c43d795b88a663f49bcfd43a5c0a127d0
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2024-09-10 21:08:53 +0200

    Generaliser kjøringen av test cases

--- system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/Main.kt
+++ system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/Main.kt
@@ -1,10 +1,16 @@
 package no.nav.soknad.arkivering
 
 import no.nav.soknad.arkivering.kafka.KafkaListener
+import no.nav.soknad.arkivering.utils.TestCase
 import org.slf4j.LoggerFactory
+import kotlin.reflect.KFunction1
 import kotlin.system.exitProcess
 
 private val logger = LoggerFactory.getLogger("no.nav.soknad.arkivering.Main")
+
+val testCases: Map<TestCase, KFunction1<LoadTests, Result<Unit>>> = mapOf(
+	TestCase.TC01 to LoadTests::`TC01 - Innsending av 10 soknader, hver med to vedlegg`,
+)
 
 fun main() {
 	logger.info("Initializing Load Tests...")
@@ -12,24 +18,29 @@ fun main() {
 	val metrics = Metrics(System.getenv("PUSH_GATEWAY_ADDRESS"))
 	var exitStatus = 0
 
-	val totalDurationTimer = metrics.totalDuration.startTimer()
-	try {
-		logger.info("Starting the Load Tests")
-
 //		loadTests.`100 simultaneous entities, 2 times 2 MB each`()
 //		loadTests.`100 simultaneous entities, 20 times 1 MB each`()
 //		loadTests.`2000 simultaneous entities, 1 times 1 byte each`()
 //		loadTests.`5 simultaneous entities, 4 times 38 MB each`()
-		val tc01Timer = metrics.testCaseDuration.labels("TC01").startTimer()
-		loadTests.`TC01 - Innsending av 10 soknader, hver med to vedlegg`()
-			.onSuccess {
-				logger.info("Test case 01 completed successfully")
+
+	val totalDurationTimer = metrics.totalDuration.startTimer()
+	try {
+		logger.info("Starting the Load Tests")
+		testCases.entries.forEach { testCaseFunction ->
+			run {
+				val testCaseId = testCaseFunction.key.name
+				val timer = metrics.testCaseDuration.labels(testCaseId).startTimer()
+				testCaseFunction.value.invoke(loadTests)
+					.onSuccess {
+						timer.observeDuration()
+						logger.info("Test case $testCaseId completed successfully")
+					}
+					.onFailure {
+						logger.error("Test case $testCaseId failed", it)
+						exitStatus = 1
+					}
 			}
-			.onFailure {
-				logger.error("Test case 01 failed", it)
-				exitStatus = 1
-			}
-		tc01Timer.observeDuration()
+		}
 
 		metrics.lastSuccess.setToCurrentTime()
 		logger.info("Finished with the Load Tests")
---
+++ system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/utils/TestCase.kt
@@ -0,0 +1,8 @@
+package no.nav.soknad.arkivering.utils
+
+enum class TestCase() {
+	TC01,
+	TC02,
+	TC03,
+	TC04,
+}
