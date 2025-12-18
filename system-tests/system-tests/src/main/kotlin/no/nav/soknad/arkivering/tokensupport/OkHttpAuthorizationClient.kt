c679b4b0e5344a8d3efa36b8ee788470780ebcbd
nilsarne <nils-arne.ringstad@nav.no>
2025-12-18 13:23:39 +0100

    Versjonsoppdateringer og omskriving av kode i bl.a.EmbeddedDockerImages.kt og OkHttpAuthorizationClient.kt

--- system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/tokensupport/OkHttpAuthorizationClient.kt
+++ system-tests/system-tests/src/main/kotlin/no/nav/soknad/arkivering/tokensupport/OkHttpAuthorizationClient.kt
@@ -1,16 +1,7 @@
 package no.nav.soknad.arkivering.tokensupport
 
-import com.nimbusds.oauth2.sdk.GrantType
-import com.nimbusds.oauth2.sdk.auth.ClientAuthenticationMethod
-import no.nav.security.token.support.client.core.ClientAuthenticationProperties
-import no.nav.security.token.support.client.core.ClientProperties
-import no.nav.security.token.support.client.core.context.JwtBearerTokenResolver
-import no.nav.security.token.support.client.core.oauth2.ClientCredentialsTokenClient
-import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
-import no.nav.security.token.support.client.core.oauth2.OnBehalfOfTokenClient
-import no.nav.security.token.support.client.core.oauth2.TokenExchangeClient
-import no.nav.security.token.support.core.context.TokenValidationContext
-import no.nav.security.token.support.core.jwt.JwtToken
+import no.nav.security.token.support.client.core.http.OAuth2HttpRequest
+import no.nav.security.token.support.client.core.http.SimpleOAuth2HttpClient
 import no.nav.soknad.arkivering.OAuth2Config
 import okhttp3.OkHttpClient
 import java.net.URI
@@ -18,25 +9,16 @@ import java.util.concurrent.TimeUnit
 
 fun createOkHttpAuthorizationClient(scopesProvider: (OAuth2Config) -> List<String>): OkHttpClient {
 	val oauth2Conf = OAuth2Config()
-	val clientProperties = ClientProperties(
-		URI.create(oauth2Conf.tokenEndpointUrl),
-		null, grantType = GrantType(oauth2Conf.grantType),
-		scopesProvider.invoke(oauth2Conf),
-		ClientAuthenticationProperties(
-			oauth2Conf.clientId,
-			ClientAuthenticationMethod(oauth2Conf.clientAuthMethod),
-			oauth2Conf.clientSecret,
-			null
-		), null, null
-	)
-	val oauth2AccessTokenService = OAuth2AccessTokenService(
-		tokenResolver = JwtBearerTokenResolver{TokenValidationContext(mapOf("token" to JwtToken("dummy") )).firstValidToken?.encodedToken},
-		onBehalfOfTokenClient = OnBehalfOfTokenClient(DefaultOAuth2HttpClient(OkHttpClient())),
-		ClientCredentialsTokenClient(DefaultOAuth2HttpClient(OkHttpClient())),
-		tokenExchangeClient = TokenExchangeClient(DefaultOAuth2HttpClient(OkHttpClient()))
+
+	val formParams = mapOf(
+		"grant-type" to oauth2Conf.grantType,
+		"client-auth-method" to oauth2Conf.clientAuthMethod,
+		"client-id" to oauth2Conf.clientId,
+		"client-secret" to oauth2Conf.clientSecret,
+		"scope" to scopesProvider.invoke(oauth2Conf).joinToString(" ")
 	)
 
-	val tokenService = TokenService(clientProperties, oauth2AccessTokenService)
+	val tokenService = createSimpleOAuth2HttpClient()
 
 	val okHttpClientTokenService = OkHttpClient().newBuilder()
 		.connectTimeout(5, TimeUnit.MINUTES)
@@ -44,7 +26,7 @@ fun createOkHttpAuthorizationClient(scopesProvider: (OAuth2Config) -> List<Strin
 		.readTimeout(5, TimeUnit.MINUTES)
 		.callTimeout(5, TimeUnit.MINUTES)
 		.addInterceptor {
-			val token = tokenService.getToken()
+			val token = tokenService.post(createOAuth2HttpRequest(URI.create(oauth2Conf.tokenEndpointUrl),formParams ))
 			val bearerRequest = it.request().newBuilder().headers(it.request().headers)
 				.header("Authorization", "Bearer ${token.access_token}").build()
 
@@ -53,3 +35,8 @@ fun createOkHttpAuthorizationClient(scopesProvider: (OAuth2Config) -> List<Strin
 
 	return okHttpClientTokenService
 }
+
+fun createSimpleOAuth2HttpClient() = SimpleOAuth2HttpClient()
+
+fun createOAuth2HttpRequest(tokenEndpointUrl: URI, formParameters: Map<String, String>) =
+	OAuth2HttpRequest(tokenEndpointUrl = tokenEndpointUrl, formParameters = formParameters )
